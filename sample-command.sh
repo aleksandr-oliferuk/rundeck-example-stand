bbd956bbe7e6b6ac3b5bcc5486d0d7a8d3bac157
Aleksandr Oliferuk <aleksandr.oliferuk@gs-labs.ru>
2021-10-16 15:00:43 +0300

    Update sample commands

--- sample-command.sh
+++ sample-command.sh
@@ -1,1 +1,11 @@
-touch /tmp/testfile-`date "+%H-%M-%S"`
+touch /tmp/testdir/testfile-`date "+%H-%M-%S"`.log
+
+#!/bin/bash
+BACKUP_DIR=/mnt/testdir-`date "+%H-%M-%S"`
+sudo mount -t nfs freebsd:/nfs_data /mnt
+sudo mkdir $BACKUP_DIR
+sudo mv /tmp/testdir/* $BACKUP_DIR/.
+sudo ls $BACKUP_DIR
+sudo umount /mnt
+
+rm -rf /nfs_data/*
